f8cf1e17b02381a5e1fc55a89404c7070ca5013d
SlavONYX <slavonixxx@gmail.com>
2023-02-13 09:19:45 +0200

    upd

---
+++ src/components/AppBar/Navigation/Navigation.jsx
@@ -0,0 +1,29 @@
+import { useSelector } from 'react-redux';
+import { getToken } from 'redux/auth/auth-selectors';
+import { Link, useLocation } from 'react-router-dom';
+import { Tabs, Tab } from '@mui/material';
+
+export const Navigation = () => {
+  const isToken = useSelector(getToken);
+  const location = useLocation();
+
+  const pathNames = ['/', '/contacts'];
+  const currentPathName = location.pathname.split('/goit-react-hw-08-phonebook')[1];
+  const currentValue = pathNames.some(e => e === currentPathName)
+    ? currentPathName
+    : false;
+
+  return (
+    <Tabs value={currentValue} aria-label="nav tabs example">
+      <Tab value="/" component={Link} to="/goit-react-hw-08-phonebook/" label="Home" />
+      {isToken && (
+        <Tab
+          value="/contacts"
+          component={Link}
+          to="/goit-react-hw-08-phonebook/contacts"
+          label="Contacts"
+        />
+      )}
+    </Tabs>
+  );
+};
